c1f994367ea1678215b6600d908a5668ca505c82
Selva Portal <selvaportal36@gmail.com>
2022-02-04 16:02:21 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [saludo, setSaludo] = useState("");
+  useEffect(() => {
+    setSaludo("¡Hello World! 😄");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="saludo"> Hello World App</h1>
+      <p className="saludo-2"> {saludo} </p>
     </div>
   );
 }
