425e6f1d6fbe488cb5f101c39401236188dfb2d0
Selva Portal <selvaportal36@gmail.com>
2022-02-04 18:55:29 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 function App() {
   const [saludo, setSaludo] = useState("");
+  
   useEffect(() => {
     setSaludo("¡Hello World! 😄");
   }, []);
   return (
     <div className="container">
       <h1 className="saludo"> Hello World App</h1>
+      
       <p className="saludo-2"> {saludo} </p>
     </div>
   );
